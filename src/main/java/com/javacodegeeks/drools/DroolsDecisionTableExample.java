8ea96a907d61d1ebe020eb8108ef4369d4a6ed7d
usman hassan <musmanhassan@gmail.com>
2016-01-04 07:19:01 +0400

    test

--- src/main/java/com/javacodegeeks/drools/DroolsDecisionTableExample.java
+++ src/main/java/com/javacodegeeks/drools/DroolsDecisionTableExample.java
@@ -18,7 +18,7 @@ public class DroolsDecisionTableExample {
 
 	public static void main(String[] args) throws Exception {
 		KnowledgeBase knowledgeBase = createKnowledgeBaseFromSpreadsheet();
-		session =  knowledgeBase.newStatelessKnowledgeSession(); // comment
+		session =  knowledgeBase.newStatelessKnowledgeSession(); // comment two
 
 		Customer customer = new Customer();
 		Product p1 = new Product("Laptop", 15000);
